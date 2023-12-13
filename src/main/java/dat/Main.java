c8e885f875756cf0f3b0160e05133d42ebe89a4d
MortenNie <cph-mn705@cphbusiness.dk>
2023-12-13 10:44:23 +0100

    LOL

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,5 +3,9 @@ package dat;
 public class Main {
     public static void main(String[] args) {
         System.out.println("LOOOOOOOOOOOOOOOOOOL!!!");
+        System.out.println("You messed up inviting me here.....");
+        
+
     }
+
 }
